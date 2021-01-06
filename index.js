4158735058d9f5abad539134aa17ac207a4ce55a
Canon Law <canon@lawcasa.com>
2021-01-06 16:00:27 -0700

    done

--- index.js
+++ index.js
@@ -323,7 +323,81 @@ module.exports = function (mongoose) {
     __v: { type: Number, default: 0 },
   });
 
+  const SEOKeywordSchema = new Schema({
+    _id: { type: Schema.ObjectId, auto: true },
+    __v: { type: Number, default: 0 },
+    keyword: { type: String, required: true },
+    rank: { type: Number, required: true },
+  });
+
+  const SEOCompetitorsSchema = new Schema({
+    _id: { type: Schema.ObjectId, auto: true },
+    practiceName: { type: String, required: true },
+    averageRank: { type: Number, default: 0 },
+    overallRank: { type: Number, default: 0 },
+    keywords: { type: [SEOKeywordSchema], required: false },
+    placeId: { type: String, required: true },
+    __v: { type: Number, default: 0 },
+  });
+
+  const PPCKeywordSchema = new Schema({
+    _id: { type: Schema.ObjectId, auto: true },
+    __v: { type: Number, default: 0 },
+    keyword: { type: String, required: true },
+    cpc: { type: Number, required: true },
+  });
+
+  const ReportSchema = new Schema({
+    _id: { type: Schema.ObjectId, auto: true },
+    owner: { type: Schema.Types.ObjectId, required: true },
+    date: { type: Date, default: new Date() },
+    data: {
+      seo: {
+        competitors: { type: [SEOCompetitorsSchema], default: [] },
+        averageRank: { type: Number },
+        overallRank: { type: Number },
+        rankings: { type: [SEOKeywordSchema], default: [] },
+        numCompetitors: { type: Number },
+      },
+      reviews: {
+        rating: { type: Number },
+        numReviews: { type: Number },
+        topFiveAvgRating: { type: Number },
+        topFiveAvgNumReviews: { type: Number },
+        competitors: { type: [ReviewCompetitorSchema], default: [] },
+      },
+      ppc: {
+        keywords: [PPCKeywordSchema],
+      },
+      __v: { type: Number, default: 0 },
+    },
+  });
+
+  const PhoneLeadSchema = new Schema({
+    _id: { type: Schema.ObjectId, auto: true },
+    owner: { type: Schema.Types.ObjectId, required: true },
+    date: { type: Date, default: new Date() },
+    wantsCall: Boolean,
+    hasCommunicationSoftware: Boolean,
+    lastWebsiteUpdate: {
+      value: Number,
+      label: String,
+    },
+    marketingBudget: {
+      value: Number,
+      label: String,
+    },
+    phoneNumber: String,
+    userName: String,
+    email: String,
+    practiceName: String,
+    zipCode: String,
+  });
+
   const Lead = mongoose.model("Lead", LeadSchema);
+
+  const Report = mongoose.model("LeadReport", ReportSchema);
+  const PhoneLead = mongoose.model("PhoneLead", PhoneLeadSchema);
 
   const Practice = mongoose.model("practice-v2", PracticeSchema);
   const Note = mongoose.model("Note", NoteSchema);
@@ -395,5 +469,7 @@ module.exports = function (mongoose) {
     CallRailMonth,
     PpcMonth,
     Lead,
+    Report,
+    PhoneLead,
   };
 };
