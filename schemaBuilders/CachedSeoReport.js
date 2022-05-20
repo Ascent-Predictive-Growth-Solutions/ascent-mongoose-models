1259d579b48787e89cd6adcd4cd43ba8b1b8311d
Harrison Sligting <hsligting@icloud.com>
2022-05-20 14:47:52 -0600

    add search parameters to cached seo report

--- schemaBuilders/CachedSeoReport.js
+++ schemaBuilders/CachedSeoReport.js
@@ -2,6 +2,20 @@ module.exports = function (mongoose) {
     return new mongoose.Schema({
         _id: { type: mongoose.Schema.ObjectId, auto: true, immutable: true },
         googleMyBusinessAccountId: { type: String, required: true },
+        searchAreaType: { type: String, enum: ["zipCode", "city"], required: true },
+        keywordSetLabel: {
+            type: String,
+            enum: [
+                "generalDentist",
+                "cosmeticDentist",
+                "pediatricDentist",
+                "prosthodontist",
+                "periodontist",
+                "orthodontist",
+                "oralSurgeon",
+            ],
+            required: true,
+        },
         date: { type: Date, required: true },
         rank: { type: Number, required: true },
     });
