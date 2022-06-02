9be621cc5e3c8faa7b3d6d56d337f85740128267
Harrison Sligting <hsligting@icloud.com>
2022-06-02 00:15:58 -0600

    update call rail calls

--- schemaBuilders/CallRailCall.js
+++ schemaBuilders/CallRailCall.js
@@ -1,25 +1,10 @@
 module.exports = function (mongoose) {
-    const tagSchema = new mongoose.Schema({ name: { type: String } });
-
     return new mongoose.Schema({
-        answered: { type: Boolean },
-        business_phone_number: { type: String },
-        customer_city: { type: String },
-        customer_country: { type: String },
-        customer_name: { type: String },
-        customer_phone_number: { type: String },
-        customer_state: { type: String },
-        direction: { type: String },
-        duration: { type: String },
-        id: { type: String },
-        recording: { type: String },
-        recording_duration: { type: Number },
-        recording_player: { type: String },
-        start_time: { type: String },
-        tracking_phone_number: { type: String },
-        voicemail: { type: Boolean },
+        trackerId: { type: String },
+        isNewPatient: { type: Boolean },
+        companyId: { type: String },
         source: { type: String },
-        tracker_id: { type: String },
-        tags: { type: [tagSchema] },
+        phoneNumber: { type: String },
+        date: { type: Date },
     });
 };
