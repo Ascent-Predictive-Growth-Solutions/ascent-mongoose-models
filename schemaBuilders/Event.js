f7d382739db3fcbc4eaddf684c7a463b4c9d653f
bsheranian <bsheranian1@gmail.com>
2022-06-15 12:46:18 -0600

    add device type field to event schema

--- schemaBuilders/Event.js
+++ schemaBuilders/Event.js
@@ -2,8 +2,9 @@ module.exports = function(mongoose) {
     return new mongoose.Schema({
         accountId: { type: String, required: true, immutable: true },
         userId: { type: String, required: true, immutable: true },
-        userType: {type: String, required: true, immutable: true },
+        userType: { type: String, required: true, immutable: true },
         timestamp: { type: Date, required: true, immutable: true },
-        eventType: {type: String, required: true, immutable: true },
+        eventType: { type: String, required: true, immutable: true },
+        deviceType: { type: String, required: true, immutable: true },
     });
 }
