3f3fffca969124366eea100542b23ed656af796c
bsheranian <bsheranian1@gmail.com>
2022-06-15 14:28:38 -0600

    change event schema userId and accountId to be ObjectId instead of String

--- schemaBuilders/Event.js
+++ schemaBuilders/Event.js
@@ -1,7 +1,7 @@
 module.exports = function(mongoose) {
     return new mongoose.Schema({
-        accountId: { type: String, required: true, immutable: true },
-        userId: { type: String, required: true, immutable: true },
+        accountId: { type: mongoose.Types.ObjectId, required: true, immutable: true },
+        userId: { type: mongoose.Types.ObjectId, required: true, immutable: true },
         userType: { type: String, required: true, immutable: true },
         timestamp: { type: Date, required: true, immutable: true },
         eventType: { type: String, required: true, immutable: true },
