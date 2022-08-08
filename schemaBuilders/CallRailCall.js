447be97254146ce5cee49d713a3410ac0edfe10d
Harrison Sligting <hsligting@icloud.com>
2022-08-08 12:56:25 -0600

    add fields to call rail calls

--- schemaBuilders/CallRailCall.js
+++ schemaBuilders/CallRailCall.js
@@ -1,4 +1,11 @@
 module.exports = function (mongoose) {
+    const TagSchema = new mongoose.Schema(
+        {
+            name: { type: String },
+        },
+        { _id: false, versionKey: false }
+    );
+
     return new mongoose.Schema({
         trackerId: { type: String },
         isNewPatient: { type: Boolean },
@@ -6,5 +13,9 @@ module.exports = function (mongoose) {
         source: { type: String },
         phoneNumber: { type: String },
         date: { type: Date },
+        duration: { type: Number },
+        tags: { type: [TagSchema], default: [] },
+        rawNumber: { type: String },
+        isAnswered: { type: Boolean },
     });
 };
