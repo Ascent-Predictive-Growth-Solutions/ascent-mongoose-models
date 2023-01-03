1c90836771c8778bc0a2add54c943db52337f45f
Harrison Sligting <hsligting@icloud.com>
2023-01-03 13:41:05 -0700

    change schema type to model

--- index.d.ts
+++ index.d.ts
@@ -1,36 +1,38 @@
-type Schema = typeof import("mongoose").Schema
+type Model = typeof import("mongoose").Model;
 
-declare function createModels(mongoose: typeof import("mongoose")): {
-    Practice: Schema
-    Note: Schema,
-    Contact: Schema,
-    DirectMailCampaign: Schema,
-    User: Schema,
-    KpiMonth: Schema,
-    SeoMonth: Schema,
-    CallRailDay: Schema,
-    Lead: Schema,
-    Report: Schema,
-    PhoneLead: Schema,
-    ClientAccount: Schema,
-    ClientUser: Schema,
-    Session: Schema,
-    NextStrategistId: Schema,
-    ProductForm: Schema,
-    PracticeManagementReport: Schema,
-    SeoForm: Schema,
-    BrandForm: Schema,
-    SocialMediaForm: Schema,
-    GoogleAdsForm: Schema,
-    WebsiteForm: Schema,
-    DomainForm: Schema,
-    SeoSummaryReport: Schema,
-    CallRailCall: Schema,
-    Event: Schema,
-    NewPatient: Schema,
-    VeloxTreatment: Schema,
-    VeloxPatient: Schema,
-    Invoice: Schema,
-}
+declare function createModels(
+    mongoose: typeof import("mongoose")
+): {
+    Practice: Model;
+    Note: Model;
+    Contact: Model;
+    DirectMailCampaign: Model;
+    User: Model;
+    KpiMonth: Model;
+    SeoMonth: Model;
+    CallRailDay: Model;
+    Lead: Model;
+    Report: Model;
+    PhoneLead: Model;
+    ClientAccount: Model;
+    ClientUser: Model;
+    Session: Model;
+    NextStrategistId: Model;
+    ProductForm: Model;
+    PracticeManagementReport: Model;
+    SeoForm: Model;
+    BrandForm: Model;
+    SocialMediaForm: Model;
+    GoogleAdsForm: Model;
+    WebsiteForm: Model;
+    DomainForm: Model;
+    SeoSummaryReport: Model;
+    CallRailCall: Model;
+    Event: Model;
+    NewPatient: Model;
+    VeloxTreatment: Model;
+    VeloxPatient: Model;
+    Invoice: Model;
+};
 
-export default createModels
+export default createModels;
